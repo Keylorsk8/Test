7d10f4cf7fc3152f86f1aef40514db4f3661c572
Keylor Martínez <kmartinez@vertafore.com>
2022-03-01 08:30:40 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 1
+    setTimeout(() => {
+        if(a == 2) {
+            resolve('Success');
+        } else {
+            reject('Failed');
+        }
+    }, 2000);
+    
+});
+
+const test = async () => {
+    let response = await p;
+    return response;
+}
+
+console.log(test());
+console.log('Test');
+
+setTimeout(() => {
+    console.log(test());
+}, 5000);
+
